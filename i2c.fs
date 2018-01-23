3be888e49feedaa4602441ba204cbdc7f635b114
Eelkhoorn <32355077+Eelkhoorn@users.noreply.github.com>
2018-01-23 11:39:35 +0100

    Update i2c.fs

--- i2c.fs
+++ i2c.fs
@@ -1,4 +1,5 @@
-\ Forth i2c words to be used with stm8ef (https://github.com/TG9541/stm8ef).
+\ Code from https://lujji.github.io/blog/bare-metal-programming-stm8/
+\ slightly adapted and ported to stm8ef
 
 #require MARKER
 
@@ -26,53 +27,78 @@ NVM
    $A0 I2C_OARL C!    \ own address 0xA0
    0 I2C_CCRH 6 B!    \ duty cycle
    1 I2C_OARH 6 B!    \ mandatory
-   $50 I2C_CCRL C!    \ i2c freq 100 kHz
+   $50 I2C_CCRL C!    \ i2c freq 100 kHz, CCR = f.master/(2 f.i2c)
    $11 I2C_TRISER C!  \ TRISER = CPU freq in MHz + 1
    1 I2C_CR1 0 B!     \ Periferal enable
    1 I2C_CR2 2 B!     \ acknoledge enable
 ;
 
-\ Check event:
-: i2c-ce ( u -- f)  
-   dup >R $0004 = if I2C_SR2 C@ $04 AND  ( last_event) 
-                      \ Slave acknowledge failure
-   else
-         I2C_SR1 C@   ( ?)          \ StatusRegister1
-         I2C_SR3 C@   ( ? ?)        \ StatusRegister3
-         $100 * or  
-   then  ( last_event)
-   R@ and R> = if -1 else 0 then
-;
 
 : i2c-start ( --)
    1 I2C_CR2 0 B!
+   BEGIN I2C_SR1 C@ 1 AND UNTIL
 ;
 
-: i2c-stop ( ? --) 
-   if begin $0684 i2c-ce until   \ slave_byte_transmitted
-   else begin $0784 i2c-ce until \ master_byte_transmitted
+: i2c-stop ( --)
    1 I2C_CR2 1 B!
+   BEGIN I2C_SR3 C@ 1 AND NOT UNTIL
 ;
 
-\ write mode send address:
-: i2c-wsa ( adr --) 
-   begin $0301 i2c-ce until      \ master_mode_select
-   2* I2C_DR C!                  \ push left shifted addr to DR
-   begin $0302 i2c-ce until ;    \ master_receiver_mode_selected
-
-\ send byte:
-: i2c-sb ( c --) 
-   begin $0780 i2c-ce            \ master_byte_transmitting
-   until I2C_DR c!
-;	         
-
-: i2c-w ( b reg-adr i2c-adr) 
-   i2c-start i2c-wsa i2c-sb i2c-sb 0 i2c-stop ;
-
-\ write n bytes:
-: i2c-wsn ( pointer n reg-adr i2c-adr --) 
-   i2c-start i2c-wsa i2c-sb 
-   0 do dup i + c@ i2c-sb loop 0 i2c-stop drop
+: i2c-w ( b --)   \ Send one byte
+   I2C_DR C!
+   BEGIN I2C_SR1 C@ $80 AND UNTIL
 ;
 
-regs
+: i2c-a ( b f --)   \ Send address write (f=0) or read (f=1) mode
+   SWAP 2* + I2C_DR C!
+   BEGIN I2C_SR1 C@ 2 AND UNTIL
+   I2C_SR3 C@ DROP 1 I2C_CR2 2 B!
+;
+
+: i2c-wb ( b reg-adr i2c-adr)   \ write one byte to register at i2c
+   i2c-start 0 i2c-a i2c-w i2c-w i2c-stop
+;
+
+\ write n bytes from buffer to reg at i2c:
+: i2c-wbf ( buf-adr n reg-adr i2c-adr --) 
+   i2c-start 0 i2c-a i2c-w 
+   0 do dup i + c@ i2c-w loop i2c-stop drop
+;
+
+: i2c-r ( -- b)   \ receive last byte
+   BEGIN I2C_SR1 C@ $40 AND UNTIL  \ RXNE, data register not empty
+   I2C_DR C@
+;
+
+: i2c-r2 ( -- b)   \ receive second last byte
+   0 I2C_CR2 2 B!
+   i2c-stop
+   i2c-r
+;   
+
+: i2c-rbf ( buf-adr n -- )   \ read two or more bytes into buffer
+    ( ba n) 2- 0 DO  \ 1 I2C_CR2 2 B!
+           BEGIN I2C_SR1 C@ 4 AND UNTIL    \ Byte Transfer Finished
+             DUP I2C_DR c@ SWAP C! 1+
+        LOOP ( n-1+ba)
+   DUP i2c-r2 SWAP C!  1+ i2c-r SWAP C!
+;
+
+: i2c-sr ( reg-adr i2c-adr -- )             \ set register for read
+   DUP >R i2c-start 0 i2c-a i2c-w i2c-stop
+   i2c-start R> 1 i2c-a
+;
+
+: i2c-rb ( reg-adr i2c-adr -- b)            \ read single byte
+   i2c-sr i2c-r2
+;
+
+: i2c-lbf ( buf-adr n reg-adr i2c-adr --)   \ load buffer
+   i2c-sr i2c-rbf 
+;
+
+variable bf 10 allot
+
+regs   \ clean up
+
+
