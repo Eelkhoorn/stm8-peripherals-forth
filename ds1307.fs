dff38f654aea2ffa119f30d38ffa2b7468fa5924
Eelkhoorn <32355077+Eelkhoorn@users.noreply.github.com>
2018-01-25 13:56:29 +0100

    Update ds1307.fs

--- ds1307.fs
+++ ds1307.fs
@@ -1,15 +1,18 @@
 \ RealTimeClock, "Tiny RTC I2C Module"
 
+RAM
+: _ ;
+
 #require MARKER
 \ #require i2c.fs
 
-$68 CONSTANT i2cadr
+$68 CONSTANT i2c-adr
 
 NVM
 
 VARIABLE bf 7 ALLOT
 
-MARKER i2c-adr
+MARKER clean
 
 \ initialise clock with day 2, 23/01/'17 13:05:00
 create buf $0005 , $1302 , $2301 , $17 C,
@@ -21,7 +24,7 @@ NVM
 
 
 : clck
-   bf 7 0 i2cadr i2c-lbf cr
+   bf 7 0 i2c-adr i2c-lbf cr
    bf 2+ C@ $3F AND h. ."  : " bf 1+ C@ h. ."  : " bf C@ h.
 ;
 
@@ -30,4 +33,4 @@ NVM
    bf 4 + C@ h. ."  / " bf 5 + C@ h. ."  / '" bf 6 + C@ h.
 ;
    
-i2c-adr
+clean
