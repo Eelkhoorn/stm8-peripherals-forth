04d47636a150f9ebb5d85f9fc9e394b77ab08d9c
Eelkhoorn <32355077+Eelkhoorn@users.noreply.github.com>
2019-01-11 16:59:14 +0100

    Update i2c.fs

--- i2c.fs
+++ i2c.fs
@@ -6,27 +6,18 @@
 RAM
 : _ ;
 #require MARKER
+#require ]B!
+#require ]C!
 
 MARKER regs
 
 \ \res MCU: STM8S103
-\ \res MCU: STM8L052
-\res export PB_DDR
-\res export PB_CR1
-\res export I2C_CR1
-\res export I2C_CR2
-\res export I2C_FREQR
-\res export I2C_OARL
-\res export I2C_OARH
-\res export I2C_CCRL
-\res export I2C_CCRH
-\res export I2C_TRISER
-\res export I2C_DR
-\res export I2C_SR1
-\res export I2C_SR2
-\res export I2C_SR3
-\res export CLK_PCKENR1  \ 50C3
-
+\res MCU: STM8L052
+\res export PB_DDR PB_CR1
+\res export I2C_CR1 I2C_CR2 I2C_FREQR I2C_OARL I2C_OARH
+\res export I2C_CCRL I2C_CCRH I2C_TRISER I2C_DR
+\res export I2C_SR1 I2C_SR2 I2C_SR3
+\res export CLK_PCKENR1
 
 NVM
 
@@ -36,18 +27,18 @@ VARIABLE SAA   \ Slave address aknowledged
 : dl 0 DO I DROP LOOP ;  
 
 : i2i ( -- ) \ initialise
-   0 I2C_CR1 0 B!     \ Periferal disable
-   1 CLK_PCKENR1 3 B! \ enable SYSCLK to I2C, needed for stm8l052
-   $80 I2C_CR2 C!     \ reset BSY
-   0 I2C_CR2 C!       
-   1 I2C_FREQR 4 B!   \ CPU freq 16 MHz
-   $A0 I2C_OARL C!    \ own address 0xA0
-   $40 I2C_OARH C!    \ 7 bit address mode
-   0 I2C_CCRH 6 B!    \ duty cycle  
-   $50 I2C_CCRL C!    \ i2c freq 100 kHz, CCR = f.master/(2 f.i2c)
-   $11 I2C_TRISER C!  \ TRISER = CPU freq in MHz + 1
-   1 I2C_CR1 0 B!     \ Periferal enable
-   1 I2C_CR2 2 B!     \ acknoledge enable
+   [ 0 I2C_CR1 0 ]B!     \ Periferal disable
+   [ 1 CLK_PCKENR1 3 ]B! \ enable SYSCLK to I2C, needed for stm8l052
+   [ $80 I2C_CR2 ]C!     \ reset BSY
+   [ 0 I2C_CR2 ]C!       
+   [ 1 I2C_FREQR 4 ]B!   \ CPU freq 16 MHz
+   [ $A0 I2C_OARL ]C!    \ own address 0xA0
+   [ $40 I2C_OARH ]C!    \ 7 bit address mode
+   [ 0 I2C_CCRH 6 ]B!    \ duty cycle  
+   [ $50 I2C_CCRL ]C!    \ i2c freq 100 kHz, CCR = f.master/(2 f.i2c)
+   [ $11 I2C_TRISER ]C!  \ TRISER = CPU freq in MHz + 1
+   [ 1 I2C_CR1 0 ]B!     \ Periferal enable
+   [ 1 I2C_CR2 2 ]B!     \ acknoledge enable
 ;
 
 : sr1 ( b --)   \ wait for sr1 flag
@@ -59,12 +50,12 @@ VARIABLE SAA   \ Slave address aknowledged
 ;
 
 : i2s ( --)   \ start
-   1 I2C_CR2 0 B!
+   [ 1 I2C_CR2 0 ]B!
 \   1 sr1
 ;
 
 : i2p ( --)   \ stop
-   1 I2C_CR2 1 B!
+   [ 1 I2C_CR2 1 ]B!
    BEGIN I2C_SR3 C@ 1 AND NOT UNTIL
 ;
 
@@ -81,15 +72,16 @@ VARIABLE SAA   \ Slave address aknowledged
 : i2a ( b f --)   \ Send address write (f=0) or read (f=1) mode
    1 sr1    \ SB cleared by mcu after writing DR
    SWAP 2* + I2C_DR C!
-   100 dl I2C_SR2 C@ 4 AND IF
+   100 dl I2C_SR2 C@ 4 AND
+   IF
      CR ." Slave address not acknowledged"
-     0 I2C_SR2 2 B!  \ reset AF
-     0 SAA C!
+     [ 0 I2C_SR2 2 ]B!  \ reset AF
+     [ 0 SAA ]C!
      i2p
      ELSE
-     1 SAA C!
+     [ 1 SAA ]C!
      2 sr1
-     cla 1 I2C_CR2 2 B!  \ clear ADDR, enable acknowledge
+     cla [ 1 I2C_CR2 2 ]B!  \ clear ADDR, enable acknowledge
    THEN
 ;
 
@@ -107,8 +99,8 @@ VARIABLE SAA   \ Slave address aknowledged
 ;
 
 : i2cb ( -- b)             \ receive single byte
-   0 I2C_CR2 2 B!            \ reset ACK
-   cla 1 I2C_CR2 1 B!	     \ set STOP
+   [ 0 I2C_CR2 2 ]B!            \ reset ACK
+   [ cla 1 I2C_CR2 1 ]B!	     \ set STOP
    $40 sr1 I2C_DR C@
 ;
 
@@ -116,10 +108,10 @@ VARIABLE SAA   \ Slave address aknowledged
 : i2cf ( buf-adr n -- )
    DUP 2 = IF
      DROP
-     1 I2C_CR2 3 B!          \ set POS
-     cla 0 I2C_CR2 2 B!      \ reset ACK
+     [ 1 I2C_CR2 3 ]B!          \ set POS
+     [ cla 0 I2C_CR2 2 ]B!      \ reset ACK
      $44 sr1
-     1 I2C_CR2 1 B!          \ set STOP
+     [ 1 I2C_CR2 1 ]B!          \ set STOP
      dr>bf $44 sr1 dr>bf
      ELSE
      cla
@@ -127,9 +119,9 @@ VARIABLE SAA   \ Slave address aknowledged
      DUP 3 = IF
        DROP $40 sr1
        dr>bf $40 sr1
-       0 I2C_CR2 2 B!        \ reset ACK
+       [ 0 I2C_CR2 2 ]B!        \ reset ACK
 	   dr>bf
-	   1 I2C_CR2 1 B!        \ set STOP
+	   [ 1 I2C_CR2 1 ]B!        \ set STOP
 	   $44 sr1 dr>bf 0
 	   ELSE
 	   $40 sr1
@@ -153,7 +145,7 @@ VARIABLE SAA   \ Slave address aknowledged
 ;
 
 \ Scan I2C addresses
-: scan 128 0 do i dup 0 i2s i2a saa c@ if cr . i2p then loop ;
+: scan &128 0 do i dup 0 i2s i2a saa c@ if cr . i2p then loop ;
 
 \ Display I2C registers
 : drg I2C_CR1 $a 0 do cr dup dup . c@ space . 1+ loop drop ;
